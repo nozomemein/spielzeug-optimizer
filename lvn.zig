79b33de89a3304ce7730e7c76f37027540e2d430
nozomemein <b8yukifsukeo999n@gmail.com>
2026-04-26 08:12:54 +0900

    .

--- lvn.zig
+++ lvn.zig
@@ -1,33 +1,32 @@
 const std = @import("std");
 
 // SSA ID
-const ValueId = u32; // type alias
+const InsnId = usize; // type alias
 
 // Insn (minimal)
 const Insn = union(enum) {
-    const_: struct { dst: ValueId, value: i64 },
-    add: struct { dst: ValueId, lhs: ValueId, rhs: ValueId },
-    sub: struct { dst: ValueId, lhs: ValueId, rhs: ValueId },
-    mul: struct { dst: ValueId, lhs: ValueId, rhs: ValueId },
-    div: struct { dst: ValueId, lhs: ValueId, rhs: ValueId },
-    copy: struct { dst: ValueId, value: ValueId },
-    ret: struct { dst: ValueId },
+    const_: struct { value: i64 },
+    add: struct { lhs: InsnId, rhs: InsnId },
+    sub: struct { lhs: InsnId, rhs: InsnId },
+    mul: struct { lhs: InsnId, rhs: InsnId },
+    div: struct { lhs: InsnId, rhs: InsnId },
+    copy: struct { value: InsnId },
+    ret: struct {},
 };
 
 // Basic Block ID
-const BlockId = u32; // type alias
+const BlockId = usize; // type alias
 // just BB
 const BasicBlock = struct {
     id: BlockId,
-    allocator: std.mem.Allocator,
-    insns: std.ArrayList(Insn),
+    insns: std.ArrayList(Insn), // FIXME: This should be ArrayList(InsnId)
 
-    pub fn init(allocator: std.mem.Allocator, id: BlockId) BasicBlock {
-        return .{ .insns = .empty, .allocator = allocator, .id = id };
+    pub fn init(id: BlockId) BasicBlock {
+        return .{ .insns = .empty, .id = id };
     }
 
-    pub fn push_insn(self: *@This(), insn: Insn) !void {
-        try self.insns.append(self.allocator, insn);
+    pub fn push_insn(self: *@This(), insn: Insn, allocator: std.mem.Allocator) !void {
+        try self.insns.append(allocator, insn);
     }
 
     // Optional if we want to release manually
@@ -40,31 +39,31 @@ const BasicBlock = struct {
 const Function = struct {
     allocator: std.mem.Allocator,
     blocks: std.ArrayList(BasicBlock),
+    insns: std.ArrayList(Insn),
 
     pub fn init(allocator: std.mem.Allocator) Function {
-        return .{ .blocks = .empty, .allocator = allocator };
+        return .{ .blocks = .empty, .insns = .empty,  .allocator = allocator };
     }
 
     pub fn push_block(self: *@This(), block: BasicBlock) !void {
         try self.blocks.append(self.allocator, block);
     }
 
-    pub fn pusn_insn(self: *@This(), block_id: BlockId, insn: Insn) !void {
-        // TODO: handle block id correctly
-        // TODO: increment SSA ID
-        std.debug.print("block id: {d}", .{block_id});
-        try self.blocks.items[0].push_insn(insn);
+    pub fn pusn_insn(self: *@This(), block_id: BlockId, insn: Insn) !InsnId {
+        const insn_id = self.insns.items.len;
+        try self.blocks.items[block_id].push_insn(insn, self.allocator);
+        return insn_id;
     }
 };
 
 pub fn main(init: std.process.Init) !void {
     const arena = init.arena.allocator();
     var function = Function.init(arena);
-    const bb = BasicBlock.init(arena, 1);
+    const bb = BasicBlock.init(0);
     try function.push_block(bb);
-    try function.pusn_insn(bb.id, .{ .const_ = .{ .dst = 1, .value = 10 } });
-    try function.pusn_insn(bb.id, .{ .const_ = .{ .dst = 2, .value = 5 } });
-    try function.pusn_insn(bb.id, .{ .add = .{ .dst = 3, .lhs = 1, .rhs = 2 } });
+    try function.pusn_insn(bb.id, .{ .const_ = .{ .value = 10 } });
+    try function.pusn_insn(bb.id, .{ .const_ = .{ .value = 5 } });
+    try function.pusn_insn(bb.id, .{ .add = .{ .lhs = 1, .rhs = 2 } });
 
     for (function.blocks.items[0].insns.items) |insn| {
         std.debug.print("{}\n", .{insn});
