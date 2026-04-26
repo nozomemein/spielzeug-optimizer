138621e855b5c9b4dfd48550f71abfd4209f236c
nozomemein <b8yukifsukeo999n@gmail.com>
2026-04-27 08:30:30 +0900

    .

--- lvn.zig
+++ lvn.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const print = std.debug.print;
 
 // Function which holds array of blocks
 const Function = struct {
@@ -11,9 +12,9 @@ const Function = struct {
     }
 
     fn create_block(self: *@This()) !BlockId {
-      const block_id = self.blocks.items.len;
-      try self.push_block(BasicBlock.init(block_id));
-      return block_id;
+        const block_id = self.blocks.items.len;
+        try self.push_block(BasicBlock.init());
+        return block_id;
     }
 
     fn push_block(self: *@This(), block: BasicBlock) !void {
@@ -28,13 +29,35 @@ const Function = struct {
     }
 
     fn dump_ir(self: @This()) !void {
-        for (self.blocks.items) |block| {
-            std.debug.print("bb{d}()\n", .{block.id});
+        for (self.blocks.items, 0..) |block, block_id| {
+            std.debug.print("bb{d}()\n", .{block_id});
             for (block.insns.items) |insn_id| {
-                // TODO: Refine format
                 const insn = self.insns.items[insn_id];
-                std.debug.print("  v{d} = {}\n", .{insn_id, insn});
+                try dump_insn(insn_id, insn);
             }
+        }
+    }
+
+    fn dump_insn(insn_id: InsnId, insn: Insn) !void {
+        switch (insn) {
+            .const_ => |payload| {
+                print("  v{d} = Const Value({d})\n", .{insn_id, payload.value});
+            },
+            .add => |payload| {
+                print("  v{d} = Add v{d}, v{d}\n", .{insn_id, payload.lhs, payload.rhs});
+            },
+            .sub => |payload| {
+                print("  v{d} = Sub v{d}, v{d}\n", .{insn_id, payload.lhs, payload.rhs});
+            },
+            .mul => |payload| {
+                print("  v{d} = Mul v{d}, v{d}\n", .{insn_id, payload.lhs, payload.rhs});
+            },
+            .div => |payload| {
+                print("  v{d} = Div v{d}, v{d}\n", .{insn_id, payload.lhs, payload.rhs});
+            },
+            else => {
+                print("  v{d} = {}\n", .{ insn_id, insn });
+            },
         }
     }
 };
@@ -57,11 +80,12 @@ const Insn = union(enum) {
 const BlockId = usize; // type alias
 // just BB
 const BasicBlock = struct {
-    id: BlockId,
     insns: std.ArrayList(InsnId),
 
-    fn init(id: BlockId) BasicBlock {
-        return .{ .insns = .empty, .id = id };
+    fn init() BasicBlock {
+        return .{
+            .insns = .empty,
+        };
     }
 
     fn push_insn(self: *@This(), insn: InsnId, allocator: std.mem.Allocator) !void {
@@ -84,6 +108,6 @@ pub fn main(init: std.process.Init) !void {
 
     try function.dump_ir();
     // for (function.blocks.items[0].insns.items) |insn| {
-    //     std.debug.print("{}\n", .{insn});
+    //     print("{}\n", .{insn});
     // }
 }
